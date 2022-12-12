a970613edd9b586c525e910da13ba7eb590e6299
WooHCode <gusdn526@naver.com>
2022-12-12 22:54:42 +0900

    Initial commit

---
+++ src/test/java/jpacrud/jpastudy/JpastudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jpacrud.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpastudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
